157e937edcd8e73c3bbe8d31692314dff8267297
Artem Bulatov <artem-bulatov777@mail.ru>
2021-03-18 02:01:40 +0300

    Alishev's lesson 21. CRUD, REST, DAO (Data Access Object). And there is SpringMVC here.

---
+++ src/main/java/ru/bulatov/crudapp/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package ru.bulatov.crudapp.dao;
+
+import org.springframework.stereotype.Component;
+import ru.bulatov.crudapp.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Artem"));
+        people.add(new Person(++PEOPLE_COUNT, "Elena"));
+        people.add(new Person(++PEOPLE_COUNT, "Dominic"));
+
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show (int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+}
