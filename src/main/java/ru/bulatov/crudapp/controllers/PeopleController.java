154d9f74a89fa3fbb1c255fe88ff532fe7c40369
Artem Bulatov <artem-bulatov777@mail.ru>
2021-03-20 00:52:42 +0300

    It was Alishev's lesson 22. I've added @ModelAttribute and page for adding new people.

--- src/main/java/ru/bulatov/crudapp/controllers/PeopleController.java
+++ src/main/java/ru/bulatov/crudapp/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.bulatov.crudapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.bulatov.crudapp.dao.PersonDAO;
+import ru.bulatov.crudapp.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -32,6 +31,18 @@ public class PeopleController {
         //получим одного человека по id из DAO и передадим на отображение в представление
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+
+        return "redirect:/people";
     }
 
 }
